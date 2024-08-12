1a004d45f23a43a7ff23c00df706f7699ac160e7
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-08-12 09:20:30 +0300

    tests/main.lua -> tests/main_spec.lua

--- lua/tests/main_spec.lua
+++ lua/tests/main_spec.lua
@@ -1,55 +1,52 @@
 local api = vim.api
+local threshold = 10
+local swipe = require("swipe")
 
 describe("swipe", function()
   it("forwards", function()
-    local swipe = require("swipe")
-    local config = {
-      threshold = 10,
-    }
-    swipe.setup(config)
+    swipe.setup({
+      threshold = threshold,
+    })
     local buffer = api.nvim_create_buf(true, true)
     local second = api.nvim_create_buf(true, true)
     api.nvim_set_current_buf(buffer)
     api.nvim_set_current_buf(second)
     api.nvim_feedkeys("<c-o>", "x", true)
-    for _ = 1, 10, 1 do
+    for _ = 1, threshold, 1 do
       swipe.scroll_right()
     end
     assert.are.same(second, api.nvim_get_current_buf())
   end)
 
   it("backwards", function()
-    local swipe = require("swipe")
     local config = {
-      threshold = 10,
+      threshold = threshold,
     }
     swipe.setup(config)
     local buffer = api.nvim_create_buf(true, true)
     local second = api.nvim_create_buf(true, true)
     api.nvim_set_current_buf(buffer)
     api.nvim_set_current_buf(second)
-    for _ = 1, 10, 1 do
+    for _ = 1, threshold, 1 do
       swipe.scroll_left()
     end
     assert.are.same(buffer, api.nvim_get_current_buf())
   end)
 
   it("timeout", function()
-    local swipe = require("swipe")
-    local config = {
-      threshold = 10,
+    swipe.setup({
+      threshold = threshold,
       timeout = 1,
-    }
-    swipe.setup(config)
+    })
     local buffer = api.nvim_create_buf(true, true)
     local second = api.nvim_create_buf(true, true)
     api.nvim_set_current_buf(buffer)
     api.nvim_set_current_buf(second)
     local co = coroutine.running()
-    for _ = 1, 10, 1 do
+    for _ = 1, threshold, 1 do
       vim.defer_fn(function()
         coroutine.resume(co)
-      end, 10)
+      end, threshold)
       swipe.scroll_left()
       coroutine.yield()
     end
